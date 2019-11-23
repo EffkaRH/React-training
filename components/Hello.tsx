f30fddc7175e1b94dc29aed279e52a70cce975ae
EffkaRH <epekarko@redhat.com>
2019-11-23 16:03:09 +0100

    Add color

--- components/Hello.tsx
+++ components/Hello.tsx
@@ -1,13 +1,14 @@
 import React, { FunctionComponent } from "react";
 
 type HelloProps = {
-  name: string;
+    name: string;
+    color?: string;
 };
 
-const Hello: FunctionComponent<HelloProps> = ({ name }) => (
-  <div style={{color:"red"}}>
-    <h1>Hello {name}</h1>
-  </div>
+const Hello: FunctionComponent<HelloProps> = ({ name, color }) => (
+    <div style={{ color: color || "brown" }}>
+        <h1>Hello {name}</h1>
+    </div>
 );
 
 export default Hello;
