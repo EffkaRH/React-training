d63a6b3a587b6d448505a4b0b16aff2e100e1213
EffkaRH <epekarko@redhat.com>
2019-11-23 16:13:24 +0100

    Add link

---
+++ pages/contact.tsx
@@ -0,0 +1,21 @@
+import Head from "next/head";
+import Link from "next/link";
+
+export default () => {
+  return (
+    <>
+      <Head>
+        <title>Contact me</title>
+      </Head>
+      <Link href="/">
+        <a>About me</a>
+      </Link>
+      <p>
+        I'm <a href="https://instagram.com/jsemnela">@jsemnela</a> on Instagram,
+        you can write me there. You can also follow my owners{" "}
+        <a href="https://instagram.com/ondrejsika">@ondrejsika</a> and{" "}
+        <a href="https://instagram.com/zuzjes">@zuzjes</a>.
+      </p>
+    </>
+  );
+};
