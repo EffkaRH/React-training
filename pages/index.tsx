e363815f2ca1f99d0f61209a6f577f55910135c1
EffkaRH <epekarko@redhat.com>
2019-11-23 16:02:53 +0100

    Add head and text

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,24 @@
 import Hello from "../components/Hello";
-
+import Head from "next/head";
 export default () => {
   return (
     <>
-      <Hello name="Eva" />
+      <Head>
+        <title>About me</title>
+      </Head>
+      <p>
+        Ahoy, I'm Nela,
+        <br />
+        I'm half labrador half swiss shepard.
+      </p>
+      <p>
+        I love water (anytime, anywhere, dirty is better than clean), mud,
+        mountines and play fetch.
+      </p>
+      <p>
+        I was born 26th of May 2015 close to Hradec Kralove. Now, I live in
+        Prague with my best owners Ondrej and Zuzka.
+      </p>
     </>
   );
 };
